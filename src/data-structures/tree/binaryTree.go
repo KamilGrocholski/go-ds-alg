fb52c43e88c9a5e546ae211e25bc89c822b8539b
Mikalsqwe <kamil.sebastian.grocholski@gmail.com>
2023-05-24 22:51:41 +0200

    compare

--- src/data-structures/tree/binaryTree.go
+++ src/data-structures/tree/binaryTree.go
@@ -5,7 +5,7 @@ type Comparison = int
 const (
 	BIGGER  Comparison = 1
 	EQUAL              = 0
-	SMALLER            = 0
+	SMALLER            = -1
 )
 
 type CompareFn[T any] func(a T, b T) Comparison
